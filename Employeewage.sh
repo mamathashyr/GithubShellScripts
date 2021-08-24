f05266c0b44e46efed3d4dbaa6d1544e49b5049f
mamathashyr <89257950+mamathashyr@users.noreply.github.com>
2021-08-24 15:52:19 +0530

    Update Employeewage.sh

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 workingdays=20
 wageperhr=20
@@ -7,22 +7,38 @@ parttime=1
 totalhrs=100
 salary=0
 
+Employeewage(){
+
 for (( days=emphr=1; days<=$workingdays && emphr<=100; days++,emphr++ ))
+
 do
+
 emp_check=$((RANDOM%3));
-	if [ $emp_check -eq $fulltime ]
+   if [ $emp_check -eq $fulltime ]
 then
-	emphr=8
+   emphr=8
 elif [ $emp_check -eq $parttime ]
 then
-	emphr=4
+   emphr=4
 else
-	emphr=0
+   emphr=0
 fi
 
-	totalhrs=$(( totalhrs + emphr ))
+totaldays=$((days))
+dailywage=$((emphr * wageperhr ))
+totalhrs=$(( totalhrs + emphr ))
 emp_wages=$(( $emphr * $wageperhr ))
-salary=$(( $salary + $emp_wages ))
+salary=$(($salary + $emp_wages))
+
 done
+
+
 echo "Employee salary :$salary"
-echo "$totalhrs"
+echo "Total hours: $totalhrs"
+echo "Daily wage is: $dailywage"
+echo "Total days are: $totaldays"
+
+}
+
+
+Employeewage
